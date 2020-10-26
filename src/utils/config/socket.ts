fc59a1e8cde668cd15e0794c78809804fc96b803
divlo <contact@divlo.fr>
2020-10-26 17:01:01 +0100

    refactor: emitToMembers and emitToAuthorizedUsers

--- src/utils/config/socket.ts
+++ src/utils/config/socket.ts
@@ -16,28 +16,50 @@ export const socket: Socket = {
   }
 }
 
-interface EmitToMembersOptions {
+interface EmitEventOptions {
   event: string
   payload: {
     action: 'create' | 'delete' | 'update'
     [key: string]: any
   }
+}
+
+interface EmitToMembersOptions extends EmitEventOptions {
   guildId: number
 }
 
-/** emits socket.io event to every connected members of the guild */
-export const emitToMembers = (options: EmitToMembersOptions): void => {
-  const { event, payload, guildId } = options
+interface EmitToAuthorizedUsersOptions extends EmitEventOptions {
+  /** tests whether the current connected userId is authorized to get the event, if the callback returns true, the server will emit the event to that user */
+  isAuthorizedCallback: (userId: number) => boolean | Promise<boolean>
+}
+
+/** emits socket.io event to every connected authorized users */
+export const emitToAuthorizedUsers = (
+  options: EmitToAuthorizedUsersOptions
+): void => {
+  const { event, payload, isAuthorizedCallback: callback } = options
   socket.io?.sockets.clients(async (_error: Error, clients: string[]) => {
     for (const clientId of clients) {
       const client = socket.io?.sockets.connected[clientId]
       const userId: number = (client as any).decoded_token?.id
-      const member = await Member.findOne({
-        where: { userId, guildId }
-      })
-      if (member != null) {
+      const isAuthorized = await callback(userId)
+      if (isAuthorized) {
         client?.emit(event, payload)
       }
+    }
+  })
+}
+
+/** emits socket.io event to every connected members of the guild */
+export const emitToMembers = (options: EmitToMembersOptions): void => {
+  emitToAuthorizedUsers({
+    event: options.event,
+    payload: options.payload,
+    isAuthorizedCallback: async userId => {
+      const member = await Member.findOne({
+        where: { userId, guildId: options.guildId }
+      })
+      return member != null
     }
   })
 }
